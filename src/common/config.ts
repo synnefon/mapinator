493f42af134e84412cff508a437ddb8d248c3535
synnefon <connorjhopkins@gmail.com>
2025-10-04 03:02:57 -0700

    tweak

--- src/common/config.ts
+++ src/common/config.ts
@@ -11,7 +11,7 @@ export interface MapGenSettings {
 export const DEFAULTS: MapGenSettings = {
   resolution: 75,
   jitter: 0.5,
-  wavelength: 0.5,
+  wavelength: 0.4,
   rainfall: 0.5,
   seaLevel: 0.5,
   shatter: 0.5,    // 0..1, default 1 for current behavior
